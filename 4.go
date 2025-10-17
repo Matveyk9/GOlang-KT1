020c5f8253bb0695f30ca0b5add228a26d96cd4e
Matveyk9 <153567537+Matveyk9@users.noreply.github.com>
2025-10-17 14:17:21 +0300

    Update 4.go

--- 4.go
+++ 4.go
@@ -1,38 +1,39 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
+func sumSlice(arr []int, ch chan int) {
+	sum := 0
+	for _, v := range arr {
+		sum += v
+	}
+	ch <- sum 
+}
+
 func main() {
-    rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 
-    const n = 10
-    arr := make([]int, n)
-    for i := 0; i < n; i++ {
-        arr[i] = rand.Intn(100) + 1
-    }
+	size := 10
+	arr := make([]int, size)
+	for i := 0; i < size; i++ {
+		arr[i] = rand.Intn(100) + 1
+	}
 
-    fmt.Println("Массив:", arr)
+	fmt.Println("Массив:", arr)
 
-    sum := 0
-    min := arr[0]
-    max := arr[0]
-    for _, v := range arr {
-        sum += v
-        if v < min {
-            min = v
-        }
-        if v > max {
-            max = v
-        }
-    }
-    avg := float64(sum) / float64(n)
+	ch := make(chan int)
 
-    fmt.Println("Сумма:", sum)
-    fmt.Println("Минимум:", min)
-    fmt.Println("Максимум:", max)
-    fmt.Printf("Среднее: %.2f\n", avg)
+	mid := len(arr) / 2
+	go sumSlice(arr[:mid], ch)
+	go sumSlice(arr[mid:], ch)
+
+	sum1 := <-ch
+	sum2 := <-ch
+
+	total := sum1 + sum2
+	fmt.Println("Сумма элементов массива:", total)
 }
