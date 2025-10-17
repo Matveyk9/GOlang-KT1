906ab201b9b030e11784c65d475c21630a325115
Matveyk9 <153567537+Matveyk9@users.noreply.github.com>
2025-10-17 14:52:48 +0300

    Update last.go

--- last.go
+++ last.go
@@ -1,1 +1,40 @@
+package main
 
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+// Уровни логирования
+const (
+	INFO  = "INFO"
+	WARN  = "WARN"
+	ERROR = "ERROR"
+)
+
+func logMessage(level, message string) {
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	logLine := fmt.Sprintf("[%s] [%s] %s\n", timestamp, level, message)
+
+	fmt.Print(logLine)
+
+	// файл для ошибок
+	file, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("Ошибка открытия файла для логов: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	// Запись логов в файл
+	if _, err := file.WriteString(logLine); err != nil {
+		fmt.Printf("Ошибка записи в файл: %v\n", err)
+	}
+}
+
+func main() {
+	logMessage(INFO, "Сервис запущен")
+	logMessage(WARN, "Тестовое предупреждение")
+	logMessage(ERROR, "Произошла ошибка!")
+}
